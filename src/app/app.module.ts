bb347f9f693f84bb371f7eaf7c74ca185af6bd64
AMukherjee9 <AMukherjee9@slb.com>
2020-08-12 12:07:23 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MyComponent } from './my/my.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { LoginComponent } from './login/login.component';
+import { MatButtonModule, MatToolbarModule, MatNativeDateModule, MatIconModule,
+   MatCardModule, MatFormFieldModule, MatInputModule, MatRadioModule, MatListModule, MatCheckboxModule } from '@angular/material';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyComponent,
+    RegistrationComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatNativeDateModule, MatDatepickerModule, MatIconModule, MatCardModule,
+    MatFormFieldModule, MatInputModule, MatRadioModule, MatListModule, MatCheckboxModule,
+    FormsModule, ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
